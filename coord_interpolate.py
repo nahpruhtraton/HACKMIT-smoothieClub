2ba0b5831b5c5282ca7adca5671b141e0a9a4a9c
notarthurphan <nahpruhtraton@gmail.com>
2019-09-14 20:59:01 -0700

    it didn't work you FREAKIN dummy; finished adding coordinates manually

--- coord_interpolate.py
+++ coord_interpolate.py
@@ -1,12 +1,14 @@
-def coord_interpolate(start: tuple, end: tuple): -> List
+def coord_interpolate(start: tuple, end: tuple):
     delta = 0.000668767523134
+    distance = ((start[0] - end[0])**2 + (start[1] - end[1])**2)**(1/2)
+    num_stops = distance / delta
     slope = (start[1] - end[1])/(start[0] - end[0])
     delta_x = delta / (slope + 1)
     delta_y = delta - delta_x
-    results = [start]
+    print(start)
     intermediate = list(start)
-    while intermediate[0] < end[0] and intermediate[1] < end[1]:
+    while num_stops and intermediate[0] < end[0] and intermediate[1] < end[1]:
         intermediate[0] += delta_x
-        intermediate[1] += delta_y
-        results.append(intermediate)
-    return results
+        intermediate[1] += delta_y 
+        print(tuple(intermediate))
+        num_stops -= 1
